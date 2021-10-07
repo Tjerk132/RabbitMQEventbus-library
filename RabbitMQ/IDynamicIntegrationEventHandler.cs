cae2eb2fa16389d7ca23cff244469fd25a264e32
Tjerk Sevenich <386599@student.fontys.nl>
2021-10-07 11:20:30 +0200

    Add project files.

---
+++ RabbitMQ/IDynamicIntegrationEventHandler.cs
@@ -0,0 +1,10 @@
+﻿using Samarasoft.Common.Events;
+using System.Threading.Tasks;
+
+namespace RabbitMQ
+{
+    public interface IDynamicIntegrationEventHandler
+    {
+        Task Handle(IntegrationEvent eventData);
+    }
+}
---
+++ Util/TypeNameExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Util
+{
+    public static class TypeNameExtensions
+    {
+        public static string GetGenericTypeName(this Type type)
+        {
+            string genericName = type.Name;
+            if (type.IsGenericType)
+            {
+                int iBacktick = genericName.IndexOf('`');
+                if (iBacktick > 0)
+                {
+                    genericName = genericName.Remove(iBacktick);
+                }
+                genericName += "<";
+                Type[] typeParameters = type.GetGenericArguments();
+                for (int i = 0; i < typeParameters.Length; ++i)
+                {
+                    string typeParamName = GetGenericTypeName(typeParameters[i]);
+                    genericName += (i == 0 ? typeParamName : "," + typeParamName);
+                }
+                genericName += ">";
+            }
+
+            return genericName;
+        }
+    }
+}
